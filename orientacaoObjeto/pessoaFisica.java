044a851e2e75d69c35f0a323c592f045f2750cb9
EndersonpereiraBarcelos <souza98120@gmail.com>
2021-09-29 21:27:25 -0300

    .

---
+++ orientacaoObjeto/pessoaFisica.java
@@ -0,0 +1,78 @@
+public class pessoaFisica {
+
+    class perfilHumano{
+
+        String nome;
+        String sobrenome;
+        String ultimonome;
+        String genero;
+        int idadae;
+        int dataNascimento;
+
+        void setnome(){
+
+
+        }
+
+        void setSobrenome(){
+
+
+        }
+
+        void setultimonome(){
+
+
+        }
+
+        void setgenero(){
+
+
+        }
+
+        void setidade(){
+
+
+        }
+
+        void setdataNascimento(){
+
+
+        }
+
+        void getnome(){
+
+
+        }
+
+        void getsobrenome(){
+
+
+        }
+
+        void getultimonome(){
+
+
+        }
+        
+        void getgenero(){
+
+
+        }
+
+        void getidade(){
+
+
+        }
+
+        void getdataNascimento(){
+
+        }
+
+    }
+
+    public static void main(String[]args){
+
+        perfilHumano perfil1 = new perfilHumano();
+    }
+    
+}
