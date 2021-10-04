f04e99c428c8bfa46dacf405ab486a1441ab3ad9
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-10-04 16:31:18 -0300

    Create Carro2.java

---
+++ orientacaoObjeto/Carro2.java
@@ -0,0 +1,130 @@
+public class Carro{
+
+
+    String marca;
+    String modelo;
+    int ano;
+    double quilometragem;
+    double velocidade;
+    boolean estado;
+    String velocimetro;
+    
+
+    void setMarca(String marca){
+        this.marca = marca;
+
+
+    }
+
+    void getMarca(){
+        System.out.println("Marca do veiculo: " + this.marca);
+
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+
+    }
+
+    void getModelo(){
+        System.out.println("Modelo do veiculo: "+ this.modelo);
+
+    }
+
+    void setAno(int ano){
+        this.ano = ano;
+
+    }
+
+    void getAno(){
+        System.out.println("Ano do veiculo: " + this.ano);
+
+
+    }
+
+    void setQuilometragem(double quilometragem){
+        this.quilometragem = quilometragem;
+
+    }
+
+    void getQuilometragem(){
+        System.out.println("Quilometragem do veiculo: "+this.quilometragem);
+
+    }
+
+    void setVelocidade(double velocidade){
+        this.velocimetro = this.velocimetro + velocidade;
+
+    }
+
+    void getVelocidade(){
+        System.out.println("A velocidade do veiculo: "+this.velocidade);
+
+    }
+/*
+    vid soetEstado(String estado){
+        this.estado = estado;
+
+    }
+    */
+    void getEstado(){
+        System.out.println("O veiculo se está ligado ou não: " +this.estado);
+    }
+ 
+    boolean ligar(){
+        if(this.estado == false){
+        this.estado = true;
+        this.velocidade = 0;
+        return true;
+        
+        }else{
+            return false;
+        }
+    }
+
+    void desligado(){
+
+        this.estado = false;
+        this.velocidade = 0;
+        System.out.println("carro desligado");
+
+
+    }
+
+    void acelerar (double velocidade){
+        if(this.estado = true){
+            //this.velocidade = 
+        }
+    }
+ 
+        
+
+    
+
+    
+    public static void main(String[]args){
+
+        Carro perfilCliente = new Carro();
+        
+        perfilCliente.setMarca("Nissan");
+        perfilCliente.getMarca();
+
+        perfilCliente.setModelo("Gtr");
+        perfilCliente.getModelo();
+
+        perfilCliente.setAno(2020);
+        perfilCliente.getAno();
+
+        perfilCliente.setQuilometragem(0);
+        perfilCliente.getQuilometragem();
+
+        perfilCliente.setVelocidade(220);
+        perfilCliente.getVelocidade();
+
+        //perfilCliente.setEstado("ligado");
+        perfilCliente.getEstado();
+   
+    }
+
+
+}
