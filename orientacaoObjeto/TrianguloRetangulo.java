7c8bf4255dbc1f255c657c30bc62105259a1efd0
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-10-06 15:47:57 -0300

    Create TrianguloRetangulo.java

---
+++ orientacaoObjeto/TrianguloRetangulo.java
@@ -0,0 +1,32 @@
+public class TrianguloRetangulo {
+    
+    double catetoOposto;
+    double catetoAdjacento;
+    double hipotenusa;
+    
+    TrianguloRetangulo(double catetoOposto, double catetoAdjacente){
+        this.catetoOposto = catetoOposto;
+        this.catetoAdjacento = catetoAdjacento;
+        
+    }
+    
+      TrianguloRetangulo(double catetoOposto, double catetoAdjacente, double hipotenusa){
+        this.catetoOposto = catetoOposto;
+        this.catetoAdjacento = catetoAdjacento;
+        this.hipotenusa = hipotenusa;
+        
+    }
+    
+      void calculo(double catetoOposto, double catetoAdjacente){
+          double area = ( catetoOposto * catetoAdjacente) /2;
+          System.out.println(area);
+      }
+      
+      void calculo(double catetoOposto, double catetoAdjacente, double hipotenusa){
+          double perimetro = ( catetoOposto * catetoAdjacente + hipotenusa) /2;
+          System.out.println(perimetro);
+      }
+    
+    
+    
+}
