3e33f3e6bb50ac35ecfe5916e4aba6ee538f93c6
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-10-06 16:46:17 -0300

    Create ElevadorResult.java

---
+++ orientacaoObjeto/ElevadorResult.java
@@ -0,0 +1,12 @@
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        
+    Elevador perfilPessoa1 = new Elevador();
+
+    perfilPessoa1.getInicializar();
+    perfilPessoa1.getEntra();
+        
+    }
+    
+}
