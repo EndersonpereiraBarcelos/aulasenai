abe696dd75646fd481b697cf4f258cd8da18d2b2
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-10-06 15:48:15 -0300

    Create JavaApplication2.java

---
+++ orientacaoObjeto/JavaApplication2.java
@@ -0,0 +1,12 @@
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+        TrianguloRetangulo tr1 = new TrianguloRetangulo(20,10);
+        TrianguloRetangulo tr2 = new TrianguloRetangulo(20,100, 30);
+        
+        tr1.calculo(10,15);
+        tr1.calculo(10,15,20);
+        
+    }
+    
+}
