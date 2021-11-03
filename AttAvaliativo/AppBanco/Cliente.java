d22aa0da750009d7227db2e3b4c15a85093587f0
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-11-03 16:03:01 -0300

    Update Cliente.java

--- AttAvaliativo/AppBanco/Cliente.java
+++ AttAvaliativo/AppBanco/Cliente.java
@@ -1,4 +1,4 @@
-package AttAvaliativo.AppBanco;
+package attAvaliativo;
 
 public class Cliente {
 
@@ -6,7 +6,8 @@ public class Cliente {
     String sobrenome;
     int cpf;
     String endereco;
-    double saldo = 0.0;
+    double saldo = 0;
+ 
 
     void setAlteraNome(String nome){
         this.nome = nome;
@@ -23,29 +24,42 @@ public class Cliente {
 
     }
 
-    void setAlteraEndereco(String endereco){
+    void setalteraEndereco(String endereco){
         this.endereco = endereco;
 
     }
 
     void getNome(){
-        System.out.println("Nome Completo: " + this.nome + this.sobrenome);
+        System.out.println("Nome Completo : " + this.nome + this.sobrenome);
+    
+    }   
+
+    /*
+    void getSobrenome() {
+        System.out.println("Sobrenome: " + this.sobrenome);
+    
     }
-
-
+        */
     void getCpf(){
-        System.out.println("CPF: " + this.cpf);
-
+        System.out.println("CPF: " +this.cpf);
     }
 
-    void getEndereco(){
-        System.out.println("Endereço: " + this.endereco);
 
+    void  getEndereco(){
+        System.out.println("Endereco: " + this.endereco);
     }
 
+    /*
     public void getSaldo(){
+        //System.out.println("Saldo: "+this.saldo);
+    }
+    */
+    void getdeposito(double quantidade){
+
+        this.saldo = saldo += quantidade;
+
         System.out.println(this.saldo);
     }
-
-
+    
+    
 }
