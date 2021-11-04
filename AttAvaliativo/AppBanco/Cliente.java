ea53856ef98b3d2031bcf8b7a07c2ab349bd6f6b
pbenderson <souza98120@gmail.com>
2021-11-04 20:48:17 -0300

    .

--- AttAvaliativo/AppBanco/Cliente.java
+++ AttAvaliativo/AppBanco/Cliente.java
@@ -31,18 +31,34 @@ public class Cliente {
         System.out.println("Endereco: " + this.endereco);
     }
 
-
+/*
     void getDeposito(double quantidade){
 
         this.saldo = saldo += quantidade;
 
         System.out.println(this.saldo);
     }
+*/
 
+public void setSacar(double valorSacado){
+    if(this.veriqueSaque(valorSacado)){
+        this.saldo = this.saldo - valorSacado;
+    }
+
+}
+
+boolean veriqueSaque(double valorASacar){
+    if(this.saldo >= valorASacar){
+        return true;
+    }else{
+        return false;
+    }
+}
     void getDadosCliente(){
         System.out.println("Nome Completo : " + this.nome + this.sobrenome);
         System.out.println("CPF: " +this.cpf);
         System.out.println("Endereco: " + this.endereco);
+        System.out.println("Saldo: " + this.saldo);
     }
     
     
--- AttAvaliativo/AppBanco/Conta.java
+++ AttAvaliativo/AppBanco/Conta.java
@@ -6,36 +6,6 @@ public class Conta {
     double saldo = 0;
     int tipo;
 
-    
-    void deposito(double quantidade){
-
-        this.saldo = saldo += quantidade;
-
-        System.out.println(this.saldo);
-    }
-   /*
-    void sacar(Double valorDeSacado){
-        if (this.verificarSaque(valorDeSacado)){
-            this.saldo = this.saldo - valorDeSacado;
-        }
-    }
-
-    boolean verificarSaque(Double valorHaSacar){
-        if (this.saldo >= valorHaSacar){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    void tranferir(Double valorDeSaque, Conta destinatario){
-        if (this.verificarSaque(valorDeSaque)){
-            this.sacar(valorDeSaque);
-        }
-    }
-
-        */
-
     void setAlteraTitular(String ClienteTitular){
         this.ClienteTitular = ClienteTitular;
     }
@@ -62,11 +32,16 @@ public class Conta {
 
 
 public static void main(String[]args){
-    Cliente perfil1 = new Cliente("Enderson ", "Pereira Barcelos", 123456789, "Vitória", 900);
+    Cliente perfil1 = new Cliente("Enderson ", "Pereira Barcelos", 123456789, "Vitória", 400);
     
 
   perfil1.getDadosCliente();
-  perfil1.getDeposito(600);
+  //perfil1.getDeposito(600);
+
+  perfil1.setSacar(200);
+  perfil1.veriqueSaque(200);
+  
+
 
     }
     
