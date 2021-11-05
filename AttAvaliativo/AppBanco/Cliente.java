af8b0b76e1aeb68e6d6712876eebc608e072d424
pbenderson <65699677+EndersonpereiraBarcelos@users.noreply.github.com>
2021-11-05 16:46:42 -0300

    Update Cliente.java

--- AttAvaliativo/AppBanco/Cliente.java
+++ AttAvaliativo/AppBanco/Cliente.java
@@ -1,65 +1,73 @@
-package AttAvaliativo.AppBanco;
+package attAvaliativo;
+
 public class Cliente {
 
     String nome;
     String sobrenome;
-    int cpf;
+    String cpf;
     String endereco;
-    double saldo = 0;
+    double saldo;
 
 
-    public Cliente(String nome, String sobrenome, int cpf, String endereco, double saldo){
+    public Cliente (String nome, String sobrenome, String cpf, String endereco){
 
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
         this.endereco = endereco;
-        this.saldo = saldo;
 
     }
-    void getNome(){
+    void Nome(){
+
         System.out.println("Nome Completo : " + this.nome + this.sobrenome);
     
     }   
 
-    void getCpf(){
+    void Cpf(){
+
         System.out.println("CPF: " +this.cpf);
+
     }
 
+    void Endereco(){
 
-    void  getEndereco(){
         System.out.println("Endereco: " + this.endereco);
+
     }
 
-/*
-    void getDeposito(double quantidade){
+    void Deposito(double quantidade){
 
         this.saldo = saldo += quantidade;
 
-        System.out.println(this.saldo);
     }
-*/
+    
+    void Sacar(double quantidade) {
 
-public void setSacar(double valorSacado){
-    if(this.veriqueSaque(valorSacado)){
-        this.saldo = this.saldo - valorSacado;
+        double	novoSaldo =	this.saldo - quantidade;	
+        this.saldo = novoSaldo;
+        System.out.println("Saldo atual: "+ this.saldo);
+
     }
+    
+    void Saldo(){
 
-}
-
-boolean veriqueSaque(double valorASacar){
-    if(this.saldo >= valorASacar){
-        return true;
-    }else{
-        return false;
-    }
-}
-    void getDadosCliente(){
-        System.out.println("Nome Completo : " + this.nome + this.sobrenome);
-        System.out.println("CPF: " +this.cpf);
-        System.out.println("Endereco: " + this.endereco);
         System.out.println("Saldo: " + this.saldo);
     }
+
+    public void transfere(Conta destino, double valor){
+
+        this.saldo = this.saldo - valor;
+        destino.saldo = destino.saldo + valor;
+    }
+
     
-    
+
+    void getDadosCliente(){
+
+        System.out.println("Nome Completo : " + this.nome + this.sobrenome);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Endereco: " + this.endereco);
+       
+    }
+
 }
