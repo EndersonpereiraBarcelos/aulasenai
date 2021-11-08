9849fdc03862bc5b7d299d0d968b1da5ea23d7ed
pbenderson <souza98120@gmail.com>
2021-11-08 20:49:50 -0300

    .

--- AttAvaliativo/AppBanco/Cliente.java
+++ AttAvaliativo/AppBanco/Cliente.java
@@ -1,8 +1,64 @@
 package AttAvaliativo.AppBanco;
 
-
 public class Cliente {
 
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String endereço;
 
+    //Contrututor
+    public Cliente(String nome, String sobrenome, String cpf, String endereço){
+
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.endereço = endereço;
+    }
+
+    //SET
+    public void alterarNome(String nome){
+        this.nome = nome;
+
+    }
+
+    public void alterarSobrenome(String sobrenome){
+        this.sobrenome = sobrenome;
+
+
+    }
+
+    public void alterarCpf(String cpf){
+        this.cpf = cpf;
+
+    }
+
+    public void alterarEndereço(String endereço){
+        this.endereço = endereço;
+
+    }
+
+    //GET
+
+    public void getNome(){
+        System.out.println("Nome completo " + this.nome + this.sobrenome);
+
+    }
+
+    public void getCpf(){
+        System.out.println("CPF: " + this.cpf);
+    }
+
+    public void getEndereço(){
+        System.out.println("Endereço: " + this.endereço);
+    }
+
+    public void dadosCliente(){
+
+        System.out.println("Nome completo " + this.nome + this.sobrenome);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Endereço: " + this.endereço);
+        
+    }
 }
  
--- AttAvaliativo/AppBanco/Conta.java
+++ AttAvaliativo/AppBanco/Conta.java
@@ -1,5 +1,18 @@
 package AttAvaliativo.AppBanco;
 public class Conta {
 
+    Cliente Titular;
+    int numero;
+    double saldo;
+    int tipo;
     
+
+    public Conta(Cliente Titular, int numero, double saldo, int tipo){
+
+        this.Titular = Titular;
+        this.numero = numero;
+        this.saldo = saldo;
+        this.tipo = tipo;
+
+    }
 }
