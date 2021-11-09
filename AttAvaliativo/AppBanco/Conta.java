58adde31736385d2a3e74956914ee406fd122b00
pbenderson <souza98120@gmail.com>
2021-11-08 21:00:31 -0300

    .

--- AttAvaliativo/AppBanco/Conta.java
+++ AttAvaliativo/AppBanco/Conta.java
@@ -15,4 +15,41 @@ public class Conta {
         this.tipo = tipo;
 
     }
+
+    public void alteraTitulo(Cliente Titular){
+        this.Titular = Titular;
+
+    }
+
+    public void alteraNumero(int numero){
+        this.numero = numero;
+
+    }
+
+    public void alteratipo(int tipo){
+
+    }
+/*
+    public String getTitular(){
+        return = this.Titular;
+    }
+    */
+
+    public int getNumero(){
+
+        return this.numero;
+    }
+
+    public int getTipo(){
+
+        return this.tipo;
+    }
+
+
+    public void main(String [] agrs){
+
+        Cliente Enderson = new Cliente("Enderson ", "Barcelos", "123.456.789.90", "Vitória");
+
+        Enderson.dadosCliente();
+    }
 }
